3808ac33aa0238b54012d138b73c698a973ebfa9
Makasya <maxytka228@gmail.com>
2025-10-05 19:55:23 +0500

    Task7

--- Tyuiu.FisherMA.Sprint4.Task7.V10.Test/DataServiceTest.cs
+++ Tyuiu.FisherMA.Sprint4.Task7.V10.Test/DataServiceTest.cs
@@ -6,15 +6,41 @@ namespace Tyuiu.FisherMA.Sprint4.Task7.V10.Test
     public class DataServiceTest
     {
         [TestMethod]
+        public void ValidConvertStringToMatrix()
+        {
+            DataService ds = new DataService();
+            string input = "695847142536";
+            int[,] expected =
+            {
+                {6, 9, 5, 8},
+                {4, 7, 1, 4},
+                {2, 5, 3, 6}
+            };
+
+            int[,] result = ds.ConvertStringToMatrix(input, 3, 4);
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Assert.AreEqual(expected[i, j], result[i, j]);
+                }
+            }
+        }
+
+        [TestMethod]
         public void ValidCalculate()
         {
             DataService ds = new DataService();
 
-            string input = "695847142536";
-            int rows = 3;
-            int cols = 4;
+            int[,] matrix =
+            {
+                {6, 9, 5, 8},
+                {4, 7, 1, 4},
+                {2, 5, 3, 6}
+            };
 
-            int result = ds.Calculate(input, rows, cols);
+            int result = ds.Calculate(matrix);
             int expected = 30; 
 
             Assert.AreEqual(expected, result);
--- Tyuiu.FisherMA.Sprint4.Task7.V10/Program.cs
+++ Tyuiu.FisherMA.Sprint4.Task7.V10/Program.cs
@@ -8,10 +8,10 @@ namespace Tyuiu.FisherMA.Sprint4.Task7.V10
         {
             DataService ds = new DataService();
 
-            Console.Title = "Спринт #4 | Выполнил: Фишер М.А. | ИИПб-25-1";
+            Console.Title = "Спринт #4 | Выполнил: Фишер М. A. | ИИПб-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #4                                                               *");
-            Console.WriteLine("* Тема: Преобразование строки в матрицу                                   *");
+            Console.WriteLine("* Тема: Преобразование строки в двумерный массив                          *");
             Console.WriteLine("* Задание #7                                                              *");
             Console.WriteLine("* Вариант #10                                                             *");
             Console.WriteLine("* Выполнил: Фишер Максим Алексеевич | ИИПб-25-1                           *");
@@ -22,11 +22,24 @@ namespace Tyuiu.FisherMA.Sprint4.Task7.V10
             Console.WriteLine("***************************************************************************");
 
             string input = "695847142536";
-            int rows = 3;
-            int cols = 4;
+            int[,] matrix = ds.ConvertStringToMatrix(input, 3, 4);
 
-            int result = ds.Calculate(input, rows, cols);
+            Console.WriteLine("* Матрица 3x4:                                                            *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
 
+            int result = ds.Calculate(matrix);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
             Console.WriteLine($"Сумма нечётных чисел: {result}");
             Console.ReadKey();
         }
