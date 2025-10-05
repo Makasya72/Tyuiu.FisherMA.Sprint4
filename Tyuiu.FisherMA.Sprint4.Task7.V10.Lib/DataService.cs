6ad38dde01736293a1f99ac947ee093730874054
Makasya <maxytka228@gmail.com>
2025-10-05 19:52:50 +0500

    Task7

--- Tyuiu.FisherMA.Sprint4.Task7.V10.Lib/DataService.cs
+++ Tyuiu.FisherMA.Sprint4.Task7.V10.Lib/DataService.cs
@@ -4,36 +4,31 @@ namespace Tyuiu.FisherMA.Sprint4.Task7.V10.Lib
 {
     public class DataService : ISprint4Task7V10
     {
+     
         public int Calculate(string input, int rows, int cols)
         {
             int[,] matrix = new int[rows, cols];
             int index = 0;
             int sum = 0;
 
+           
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    if (index < input.Length)
-                    {
-                        int num = int.Parse(input[index].ToString());
-                        matrix[i, j] = num;
-                        index++;
+                    int num = int.Parse(input[index].ToString());
+                    matrix[i, j] = num;
 
-                        if (num % 2 != 0)
-                        {
-                            sum += num;
-                        }
+                    if (num % 2 != 0)
+                    {
+                        sum += num;
                     }
+
+                    index++;
                 }
             }
 
             return sum;
-        }
-
-        public int Calculate(int n, int m, string value)
-        {
-            throw new NotImplementedException();
         }
     }
 }
